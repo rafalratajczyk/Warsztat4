68005863db56f9776775c3c4d223cd72c1c0a2c0
Pawel Nowak <kontakt@nowakpawel.pl>
2017-06-22 17:41:44 +0200

    Carousel

--- web/index.php
+++ web/index.php
@@ -3,6 +3,31 @@
     require_once '../config.php';
 
     $products = Product::findAll($conn);
+    $productsCounter = count($products);
+
+    $carouselProducts = [];
+
+    if ($productsCounter > 5) {
+        //$c = 0;
+        while (count($carouselProducts) < 5) {
+            $rand = rand(0, $productsCounter-1);
+
+            for ($i = 0; $i < $productsCounter-1; $i++) {
+                if (!in_array($products[$rand], $carouselProducts)) {
+                    $carouselProducts [] = $products[$rand];
+                }
+            }
+
+            //$c++;
+        }
+        echo '<div class="carousel">';
+        foreach ($carouselProducts as $cProduct) {
+            echo '<p>'.$cProduct->getName().'</p>';
+        }
+        echo '</div>';
+    }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +35,16 @@
 <head>
     <meta charset="UTF-8">
     <title>Shop</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <style>
+        .carousel {
+            width: 50%;
+            margin-top: 20px;
+            margin-left: 25%;
+            margin-right: 25%;
+            border: 1px dashed #000;
+        }
+    </style>
 </head>
 <body>
 <?php
