787a9b01968345ffad049255091146a0ab133591
Pawel Nowak <kontakt@nowakpawel.pl>
2017-06-25 06:18:52 +0200

    Tables created - nolonger neccessary files

--- datas/categories_array.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: pawel
- * Date: 23.06.17
- * Time: 13:07
- */
-$categories =
--- datas/users_Array.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: pawel
- * Date: 23.06.17
- * Time: 14:43
- */
